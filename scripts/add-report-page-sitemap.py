d110421fee7bb9076ce510841dade0797e96303c
Big <162275628+DopestT@users.noreply.github.com>
2026-09-11 19:42:08 -0400

    Remove temporary sitemap patch script

--- scripts/add-report-page-sitemap.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-p=Path('sitemap.xml')
-s=p.read_text()
-url='https://letsgovotethistime.com/report-election-problem'
-if url not in s:
-    block='''  <url>\n    <loc>https://letsgovotethistime.com/report-election-problem</loc>\n    <lastmod>2026-09-11</lastmod>\n    <changefreq>weekly</changefreq>\n    <priority>0.90</priority>\n  </url>\n'''
-    marker='  <url>\n    <loc>https://letsgovotethistime.com/check-in</loc>'
-    s=s.replace(marker,block+marker,1)
-p.write_text(s)
